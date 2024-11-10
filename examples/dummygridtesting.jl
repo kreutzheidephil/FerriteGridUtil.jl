6178ce001d0afbdb0c5a606131e2a8ba4abfe7a4
Phil Kreutzheide <p.kreutzheide@tu-braunschweig.de>
2024-11-10 19:35:36 +0100

    tests for get_coordinate_limits() working, possible clean up needed

--- examples/dummygridtesting.jl
+++ examples/dummygridtesting.jl
@@ -7,8 +7,15 @@ nz = 5
 order = 1
 # x = Vec(-1/2, -1/2, 0.1)
 x = [-1/2, -1/2, 1/2]
+dim = 3
 
-grid = Ferrite.generate_grid(Hexahedron, (nx, ny, nz), Vec{3}((-1/2, -1/2, -1/2)), Vec{3}((1/2, 1/2, 1/2)))
+corners = Dict([ # Points such that: volume==1, center_of_mass==one(Vec{dim})
+    1 => [Vec{1}((0.5,)), Vec{1}((1.5,))],
+    2 => [Vec{2}((0.5,0.5)), Vec{2}((1.5,1.5))],
+    3 => [Vec{3}((0.5,0.5,0.5)), Vec{3}((1.5,1.5,1.5))]
+    ])
+
+grid = Ferrite.generate_grid(Hexahedron, (nx, ny, nz), corners[dim][1], corners[dim][2])
 
 ipu = Lagrange{RefHexahedron, order}()^3
 ipp = Lagrange{RefHexahedron, order}()
@@ -16,12 +23,8 @@ dh = DofHandler(grid)
 add!(dh, :u, ipu)
 add!(dh, :p, ipp)
 close!(dh)
-dofs = FerriteGridUtil.get_dofs_from_coordinate(dh, x, :u)
+# dofs = FerriteGridUtil.get_dofs_from_coordinate(dh, x, :u)
 limits=get_coordinate_limits(grid)
-corners = Dict([ # Points such that: volume==1, center_of_mass==one(Vec{dim})
-    1 => [Vec{1}((0.5,)), Vec{1}((1.5,))],
-    2 => [Vec{2}((0.5,0.5)), Vec{2}((1.5,0.5)), Vec{2}((1.5,1.5)), Vec{2}((0.5,1.5))],
-    3 => [Vec{3}((0.5,0.5,0.5)), Vec{3}((1.5,1.5,1.5))]
-    ])
-# Tuple( (minimum(corners[3][][i]), maximum(corners[3][1][i])) for i in 1:3 )
-grid = Ferrite.generate_grid(Quadrilateral, (nx, ny), Vec{2}((-1/2, -1/2)), Vec{2}((1/2, 1/2)))
+
+test_limits = Tuple( (minimum(corners[dim][1][i]), maximum(corners[dim][2][i])) for i in 1:dim )
+# grid = Ferrite.generate_grid(Quadrilateral, (nx, ny), Vec{2}((-1/2, -1/2)), Vec{2}((1/2, 1/2)))
--- test/test_property.jl
+++ test/test_property.jl
@@ -1,16 +1,16 @@
 @testset "get_moment()" begin
     @test isdefined(FerriteGridUtil, :get_moment)
-    # @testset "0ᵗʰ moment for grid with $(T) cells" for (T, grid) in simplegrids
-    #     @test isapprox(get_moment(grid, 0), 1.0; rtol=1e-8)
-    # end
-    # @testset "1ˢᵗ moment for grid with $(T) cells" for (T, grid) in simplegrids
-    #     onevec = Vec{Ferrite.getspatialdim(grid)}(i -> 1.0)
-    #     @test isapprox(get_moment(grid, 1), onevec; rtol=1e-8)
-    # end
-    # @testset "2ⁿᵈ moment for grid with $(T) cells" for (T, grid) in simplegrids
-    #     MomentTensor = SymmetricTensor{2,Ferrite.getspatialdim(grid)}((i,j) -> i==j ? 13/12 : 1)
-    #     @test isapprox(get_moment(grid, 2), MomentTensor; rtol=1e-8)
-    # end
+    @testset "0ᵗʰ moment for grid with $(T) cells" for (T, grid) in simplegrids
+        @test isapprox(get_moment(grid, 0), 1.0; rtol=1e-8)
+    end
+    @testset "1ˢᵗ moment for grid with $(T) cells" for (T, grid) in simplegrids
+        onevec = Vec{Ferrite.getspatialdim(grid)}(i -> 1.0)
+        @test isapprox(get_moment(grid, 1), onevec; rtol=1e-8)
+    end
+    @testset "2ⁿᵈ moment for grid with $(T) cells" for (T, grid) in simplegrids
+        MomentTensor = SymmetricTensor{2,Ferrite.getspatialdim(grid)}((i,j) -> i==j ? 13/12 : 1)
+        @test isapprox(get_moment(grid, 2), MomentTensor; rtol=1e-8)
+    end
 end
 
 ###################################################################################################
@@ -18,9 +18,13 @@ end
 # TODO: change 2D grid generation to using two vectors, then write test for limits 
 @testset "get_coordinate_limits()" begin
     @test isdefined(FerriteGridUtil, :get_coordinate_limits)
-    @testset "$(dim)D grid limits with $(T) cells" for (T, grid) in simplegrids
-        test_limits = Tuple( (minimum(corners[Ferrite.getspatialdim(grid)][1][i]), maximum(corners[Ferrite.getspatialdim(grid)][1][i])) for i in 1:3 )
-        @test isapprox(get_coordinate_limits(grid), test_limits; rtol=1e-8)
+    @testset "$(Ferrite.getspatialdim(grid))D grid limits with $(T) cells" for (T, grid) in simplegrids
+        dim = Ferrite.getspatialdim(grid)
+        test_limits = Tuple( (minimum(corners[dim][1][i]), maximum(corners[dim][2][i])) for i in 1:dim )
+        limits = get_coordinate_limits(grid)
+        for d in 1:dim
+            @test isapprox(collect(limits[d]), collect(test_limits[d]); rtol=1e-8)
+        end
     end
 end
 
