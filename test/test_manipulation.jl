44d12d7c69f56a7ac276d9607ae1b05866bf4494
Phil Kreutzheide <p.kreutzheide@tu-braunschweig.de>
2024-11-17 14:55:52 +0100

    further testing of manipulation fcns

--- test/test_manipulation.jl
+++ test/test_manipulation.jl
@@ -1,9 +1,20 @@
 @testset "scale_relative(), scale_relative!()" begin
-    scalefactor = rand()
-    refpoint = rand(Vec{3})
+    
     # grid_scaled = FerriteGridUtil.scale_relative(grid, scalefactor; refpoint)
     @test isdefined(FerriteGridUtil, :scale_relative) 
-    @test isdefined(FerriteGridUtil, :scale_relative!) 
+    @test isdefined(FerriteGridUtil, :scale_relative!)
+    @testset "scale_relative() for grid with $(T) cells" for (T, grid) in simplegrids
+        dim = Ferrite.getspatialdim(grid)
+        scalefactor = rand(-11:2:11)*rand()
+        refpoint = rand(Vec{3})
+        grid_scaled = scale_relative(grid, scalefactor; refpoint=refpoint)
+
+        # for (n, n_scaled) in zip(grid.nodes, grid_shifted.nodes)
+        #     @test (n_shifted.x - n.x  ≈ shiftvalue)
+        # end
+        @test get_moment(grid, 0)*scalefactor^dim ≈ get_moment(grid_scaled, 0)
+        @test get_moment(grid, 1) ≈ get_moment(grid_scaled, 1) - (refpoint + scalefactor*(onevec{dim} - refpoint))
+    end
 end
 
 ###################################################################################################
@@ -14,7 +25,7 @@ end
     @test isdefined(FerriteGridUtil, :shift_by!)
     
     @testset "shift_by() for grid with $(T) cells" for (T, grid) in simplegrids
-        shiftvalue = rand(Vec{Ferrite.getspatialdim(grid)})
+        shiftvalue = rand(-11:2:11)*rand(Vec{Ferrite.getspatialdim(grid)})
         grid_shifted = shift_by(grid, shiftvalue)
         # testthis = collect(n_shifted.x - n.x for (n, n_shifted) in zip(grid.nodes, grid_shifted.nodes))
         # @test testthis ≈ shiftvalue
