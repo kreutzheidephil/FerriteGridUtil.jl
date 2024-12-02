c42acad88a382de6490be2868a0643c59fbbee65
David Rollin <d.rollin@tu-braunschweig.de>
2024-12-02 14:55:11 +0100

    Tests for save and load

--- test/test_saveload.jl
+++ test/test_saveload.jl
@@ -1,10 +1,14 @@
-@testset "save()" begin
-    @test isdefined(FerriteGridUtil, :save) 
+testdir = mktempdir()
+@testset "Save and load grid with cells of type $(celltype)" for (celltype, grid) in simplegrids
+    filepath = joinpath(testdir, "Test_" * string(celltype) * ".h5")
+    @test ! isfile(filepath)
+    save(grid, filepath)
+    @test isfile(filepath)
+    grid₂ = load(filepath)
+    @test grid.cells == grid₂.cells
+    @test grid.nodes == grid₂.nodes
+    @test grid.cellsets  == grid₂.cellsets
+    @test grid.facetsets == grid₂.facetsets
+    @test grid.nodesets  == grid₂.nodesets
 end
-
-###################################################################################################
-###################################################################################################
-
-@testset "load()" begin
-    @test isdefined(FerriteGridUtil, :load) 
-end
+rm(testdir; recursive=true)
