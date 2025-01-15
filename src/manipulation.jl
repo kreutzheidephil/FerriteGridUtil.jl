041d38763f5756c03bda9ea3df8f5a6681b8fccf
Phil Kreutzheide <p.kreutzheide@tu-braunschweig.de>
2025-01-15 17:04:52 +0100

    docs update and clean up

--- src/manipulation.jl
+++ src/manipulation.jl
@@ -1,15 +1,15 @@
 # TODO: test, document
 """
-    scale_relative(grid::Grid{dim}, scalefactor::Real; refpoint::Vec{dim}=zero(Vec{dim})) where {dim}
+    scale_relative(grid::Grid, scalefactor::Real; refpoint::Vec)
 
-Return a grid scaled by `scalefactor` relative to a point `refpoint`. The default `refpoint` is `[0.0, 0.0, 0.0]`.
+Return a grid scaled by `scalefactor` relative to a point `refpoint`. The default `refpoint` is the `[0.0, 0.0, 0.0]`.
 """
 function scale_relative(grid::Grid{dim}, scalefactor::Real; refpoint::Vec{dim}=zero(Vec{dim})) where {dim}
     return scale_relative!(deepcopy(grid), scalefactor; refpoint=refpoint)
 end
 
 """
-    scale_relative!()
+    scale_relative!(grid::Grid, scalefactor::Real; refpoint::Vec)
 
 Scale the grid `grid` by a factor `scalefactor` relative to a point `refpoint`. The default `refpoint` is `[0.0, 0.0, 0.0]`.
 """
@@ -24,7 +24,7 @@ end
 ###################################################################################################
 #TODO: test, document
 """
-    shift_by(grid::Grid{dim}, shiftvalue::Vec{dim}) where {dim}
+    shift_by(grid::Grid, shiftvalue::Vec)
 
 Return a grid shifted by `shiftvalue`. 
 """
@@ -33,7 +33,7 @@ function shift_by(grid::Grid{dim}, shiftvalue::Vec{dim}) where {dim}
 end
 
 """
-    shift_by!(grid::Grid{dim}, shiftvalue::Vec{dim}) where {dim}
+    shift_by!(grid::Grid, shiftvalue::Vec)
 
 Shift the grid `grid` by a value `shiftvalue`.
 """
